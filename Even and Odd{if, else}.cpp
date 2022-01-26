91548b3066dedab1d889af8b50d70dd8069a7afb
Ankit Kumar <97950192+D-ekU@users.noreply.github.com>
2022-01-26 14:48:42 +0530

    Create Even and Odd{if, else}.cpp

---
+++ Even and Odd{if, else}.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a;
+  cout<<"Enter a number: ";
+  cin>>a;
+  if(a%2==0){
+    cout<<"The number " <<a<<" is Even";
+	}
+  else{
+    cout<<"The number " <<a<<" is Odd"; 
+  }
+   return 0;
+}
